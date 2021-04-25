761001ba2c1790d0e653996068c588c974ca91c8
Zhang Yi3 <yi3.zhang@intel.com>
2021-04-25 16:55:31 +0800

    fix dropout & add conversion example for mobilenetv3

--- ngraph/frontend/paddlepaddle/src/op/dropout.cpp
+++ ngraph/frontend/paddlepaddle/src/op/dropout.cpp
@@ -24,9 +24,8 @@ namespace op {
 
 NamedOutputs dropout (const NodeContext& node) {
     auto data = node.get_ng_input("X");
-    auto dropout_prob = ngraph::opset6::Constant::create(ngraph::element::f32, {1}, {node.get_attribute<float>("dropout_prob")});
+    auto dropout_prob = ngraph::opset6::Constant::create(ngraph::element::f32, {1}, {1 - node.get_attribute<float>("dropout_prob")});
     return node.default_single_output_mapping({std::make_shared<ngraph::opset6::Multiply>(data, dropout_prob)}, {"Out"});
-    //return node.default_single_output_mapping({std::make_shared<ngraph::opset6::Concat>(data, axis)}, {"Out"});
 }
 
 }}}}
