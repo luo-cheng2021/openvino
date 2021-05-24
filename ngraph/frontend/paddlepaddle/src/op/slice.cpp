0433240fd786f562143688373c8a5c3fbf601889
Luo Cheng <cheng.luo@intel.com>
2021-05-24 08:35:14 +0800

    slice support tensor list

--- ngraph/frontend/paddlepaddle/src/op/slice.cpp
+++ ngraph/frontend/paddlepaddle/src/op/slice.cpp
@@ -1,18 +1,6 @@
-//*****************************************************************************
-// Copyright 2017-2021 Intel Corporation
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//*****************************************************************************
 
 #include <ngraph/opsets/opset6.hpp>
 #include "slice.hpp"
@@ -26,30 +14,45 @@ namespace op {
 NamedOutputs slice (const NodeContext& node) {
     auto data = node.get_ng_input("Input");
     auto axes = node.get_attribute<std::vector<int32_t>>("axes");
-    auto starts = node.get_attribute<std::vector<int32_t>>("starts");
-    auto ends = node.get_attribute<std::vector<int32_t>>("ends");
-    auto parialShape = data.get_partial_shape();
-    PDPD_ASSERT(parialShape.is_static(), "slice: must use static shape.");
-    auto shape = parialShape.to_shape();
-    std::vector<int32_t> fixedStarts(shape.size(), 0);
-    std::vector<int32_t> fixedEnds(shape.size(), INT_MAX);
-    int n = 0;
-    for (size_t &&i : axes) {
-        PDPD_ASSERT(i < shape.size(), "slice: axes must be less than the X rank.");
-        fixedStarts[i] = starts[n];
-        fixedEnds[i] = ends[n];
-        n++;
+    Output<Node> start_idx_node, end_idx_node;
+    if (node.has_ng_input("StartsTensor")) {
+        start_idx_node = node.get_ng_input("StartsTensor");
+    } else if (node.has_ng_input("StartsTensorList")) {
+        auto inputs = node.get_ng_inputs("StartsTensorList");
+        start_idx_node = std::make_shared<ngraph::opset6::Concat>(inputs, 0);
+    } else {
+        auto starts = node.get_attribute<std::vector<int32_t>>("starts");
+        start_idx_node = opset6::Constant::create(element::i32, {starts.size()}, starts);
     }
-    
-    auto startsNode = ngraph::opset6::Constant::create(ngraph::element::i32, { shape.size() }, fixedStarts);
-    auto endsNode = ngraph::opset6::Constant::create(ngraph::element::i32, { shape.size() }, fixedEnds);
-    auto stridesNode = ngraph::opset6::Constant::create(ngraph::element::i32, { shape.size() }, std::vector<int32_t>(shape.size(), 1));
+
+    if (node.has_ng_input("EndsTensor")) {
+        end_idx_node = node.get_ng_input("EndsTensor");
+    } else if (node.has_ng_input("EndsTensorList")) {
+        auto inputs = node.get_ng_inputs("EndsTensorList");
+        end_idx_node = std::make_shared<ngraph::opset6::Concat>(inputs, 0);
+    } else {
+        auto ends = node.get_attribute<std::vector<int32_t>>("ends");
+        end_idx_node = opset6::Constant::create(element::i32, {ends.size()}, ends);
+    }
+
+    // the shape of input, such as [1, 1, 3, 3]
+    auto shape_node = std::make_shared<opset6::ShapeOf>(data, element::Type_t::i32);
+    // the input dim, such as [4]
+    auto shape_shape_node = std::make_shared<opset6::ShapeOf>(shape_node, element::i32);
+    auto const_0_node = opset6::Constant::create(element::i32, {}, {0});
+    auto const_max_node = opset6::Constant::create(element::i32, {}, {INT_MAX});
+    // array [0:max)
+    auto start_node = std::make_shared<opset6::Broadcast>(const_0_node, shape_shape_node);
+    auto end_node = std::make_shared<opset6::Broadcast>(const_max_node, shape_shape_node);
+    auto axes_node = opset6::Constant::create(element::i32, {axes.size(), 1}, axes);
+    auto fixed_start_node = std::make_shared<opset6::ScatterNDUpdate>(start_node, axes_node, start_idx_node);
+    auto fixed_end_node = std::make_shared<opset6::ScatterNDUpdate>(end_node, axes_node, end_idx_node);
+
     return node.default_single_output_mapping({std::make_shared<ngraph::opset6::StridedSlice>(data,
-        startsNode, 
-        endsNode, 
-        stridesNode,
-        std::vector<int64_t>(shape.size(), 0),
-        std::vector<int64_t>(shape.size(), 0))}, {"Out"});
+        fixed_start_node, 
+        fixed_end_node, 
+        std::vector<int64_t>{},
+        std::vector<int64_t>{})}, {"Out"});
 }
 
 }}}}
--- ngraph/test/files/paddlepaddle/gen_scripts/generate_slice.py
+++ ngraph/test/files/paddlepaddle/gen_scripts/generate_slice.py
@@ -27,12 +27,43 @@ def slice(name : str, x, axes : list, start : list, end : list):
 
     return outs[0]
 
+def slice_tensor(name : str, x, axes : list, start : list, end : list, use_tensor_in_list : bool):
+    pdpd.enable_static()
+
+    with pdpd.static.program_guard(pdpd.static.Program(), pdpd.static.Program()):
+        node_x = pdpd.static.data(name='x', shape=x.shape, dtype = data_type)
+        if use_tensor_in_list:
+            start[0] = pdpd.assign(np.array((start[0],)).astype('int32'))
+            end[0] = pdpd.assign(np.array((end[0],)).astype('int32'))
+            out = pdpd.fluid.layers.slice(node_x, axes=axes, starts=start, ends=end)
+        else:
+            out = pdpd.fluid.layers.slice(node_x, axes = axes, \
+                starts = pdpd.assign(np.array(start).astype('int32')), \
+                ends = pdpd.assign(np.array(end).astype('int32')))
+
+        cpu = pdpd.static.cpu_places(1)
+        exe = pdpd.static.Executor(cpu[0])
+        # startup program will call initializer to initialize the parameters.
+        exe.run(pdpd.static.default_startup_program())
+
+        outs = exe.run(
+            feed={'x': x},
+            fetch_list=[out])             
+
+        saveModel(name, exe, feedkeys=['x'], fetchlist=[out], inputs=[x], outputs=[outs[0]])
+
+    return outs[0]
+
 def main():
     x = np.linspace(1, 60, num = 60, dtype=np.int32).reshape(4, 3, 5).astype(data_type)
     slice("slice", x, axes=[1, 2], start=(0, 1), end=(-1, 3))
 
     x = np.linspace(1, 60, num = 60, dtype=np.int32).reshape(2, 30).astype(data_type)
     slice("slice_1d", x, axes=[0], start=[0], end=[1])
+
+    x = np.linspace(1, 60, num = 60, dtype=np.int32).reshape(4, 3, 5).astype(data_type)
+    slice_tensor("slice_tensor", x, axes=[1, 2], start=(0, 1), end=(-1, 3), use_tensor_in_list=False)
+    slice_tensor("slice_tensor_list", x, axes=[1, 2], start=[0, 1], end=[-1, 3], use_tensor_in_list=True)
 
 if __name__ == "__main__":
     main()
