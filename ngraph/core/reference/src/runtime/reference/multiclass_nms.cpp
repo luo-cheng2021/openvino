a1f9b200b1a6a13c0aa933a09f5fb05f8c819042
Zhang Yi3 <yi3.zhang@intel.com>
2021-06-24 17:48:01 +0800

    debug & fix

--- ngraph/core/reference/src/runtime/reference/multiclass_nms.cpp
+++ ngraph/core/reference/src/runtime/reference/multiclass_nms.cpp
@@ -368,7 +368,7 @@ namespace ngraph
 
                 if (sort_result_across_batch)
                 { /* sort across batch */
-                    if (sort_result_type == op::v8::MulticlassNms::SortResultType::SCORE)
+                    if (sort_result_type == op::v8::MatrixNms::SortResultType::SCORE)
                     {
                         std::sort(
                             filteredBoxes.begin(),
@@ -382,7 +382,7 @@ namespace ngraph
                                         l.class_index == r.class_index && l.index < r.index);
                             });
                     }
-                    else if (sort_result_type == op::v8::MulticlassNms::SortResultType::CLASSID)
+                    else if (sort_result_type == op::v8::MatrixNms::SortResultType::CLASSID)
                     {
                         std::sort(filteredBoxes.begin(),
                                   filteredBoxes.end(),
