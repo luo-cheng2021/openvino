06f0bb77f944b86bb9b5754302b505368f6e6085
Luo Cheng <cheng.luo@intel.com>
2021-10-26 08:16:34 +0800

    add xsimd support

---
+++ inference-engine/src/mkldnn_plugin/xsimd.h
@@ -0,0 +1,28 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#ifndef __XSIMD_H__
+#define __XSIMD_H__
+
+#define XSIMD_CONFIG_HPP
+#define XSIMD_WITH_SSE2 1
+#define XSIMD_WITH_SSE3 1
+#define XSIMD_WITH_SSSE3 1
+#define XSIMD_WITH_SSE4_1 1
+#define XSIMD_WITH_SSE4_2 1
+#define XSIMD_WITH_AVX 1
+#define XSIMD_WITH_AVX2 1
+#define XSIMD_WITH_FMA3 0
+#define XSIMD_WITH_AVX 1
+#define XSIMD_WITH_AVX2 1
+#define XSIMD_WITH_FMA5 1
+#define XSIMD_WITH_AVX512F 1
+#define XSIMD_WITH_AVX512CD 1
+#define XSIMD_WITH_AVX512DQ 1
+#define XSIMD_WITH_AVX512BW 1
+#define XSIMD_WITH_NEON 0
+#define XSIMD_WITH_NEON64 0
+#include "xsimd/xsimd.hpp"
+
+#endif
