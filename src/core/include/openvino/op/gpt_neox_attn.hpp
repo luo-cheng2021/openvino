fb36b006e376b5554307bf54dc0ce7cb705b9796
Luo Cheng <cheng.luo@intel.com>
2023-02-09 23:41:49 +0530

    ngraph attention skeleton

---
+++ src/core/include/openvino/op/gpt_neox_attn.hpp
@@ -0,0 +1,60 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "openvino/op/op.hpp"
+
+namespace ov {
+namespace op {
+namespace v10 {
+/// \brief      Parameterized, part of GPT Neox attention operation.
+///
+/// \ingroup ov_ops_cpp_api
+class OPENVINO_API GPTNeoxAttn : public Op {
+public:
+    OPENVINO_OP("GPTNeoxAttn", "opset10");
+
+    GPTNeoxAttn() = default;
+
+    /// \brief      Constructs operation.
+    ///
+    /// \param      data   Input tensor.
+    ///
+    GPTNeoxAttn(const Output<Node>& qkv, const Output<Node>& past_keys, const Output<Node>& past_keys_num,
+        int layer_num = 32,
+        int head_num = 32,
+        int size_per_head = 80,
+        int hidden_size = 32 * 80,
+        int intermediate_size = 10240,
+        float layer_norm_eps = 1e-5,
+        int max_position_embeddings = 2048,
+        int rotary_emb_base = 10000,
+        float rotary_pct = 0.25,
+        bool use_parallel_residual = true,
+        int vocab_size = 50304,
+        int max_seq_len = 400,
+        int cur_layer_num = 0);
+
+    bool visit_attributes(AttributeVisitor& visitor) override;
+    void validate_and_infer_types() override;
+    std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
+
+    int m_layer_num = 32;
+    int m_head_num = 32;
+    int m_size_per_head = 80;
+    int m_hidden_size = 32 * 80;
+    int m_intermediate_size = 10240;
+    float m_layer_norm_eps = 1e-5;
+    int m_max_position_embeddings = 2048;
+    int m_rotary_emb_base = 10000;
+    float m_rotary_pct = 0.25;
+    bool m_use_parallel_residual = true;
+    int m_vocab_size = 50304;
+    int m_max_seq_len = 400;
+    int m_cur_layer_num = 0;
+};
+}  // namespace v10
+}  // namespace op
+}  // namespace ov
---
+++ src/core/src/op/gpt_neox_attn.cpp
@@ -0,0 +1,78 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/op/gpt_neox_attn.hpp"
+
+#include "itt.hpp"
+
+namespace ov {
+op::v10::GPTNeoxAttn::GPTNeoxAttn(const Output<Node>& qkv, const Output<Node>& past_keys, const Output<Node>& past_keys_num,
+        int layer_num, int head_num, int size_per_head, int hidden_size, int intermediate_size,
+        float layer_norm_eps, int max_position_embeddings, int rotary_emb_base, float rotary_pct,
+        bool use_parallel_residual, int vocab_size, int max_seq_len, int cur_layer_num) :
+        op::Op({qkv, past_keys, past_keys_num}),
+        m_layer_num(layer_num),
+        m_head_num(head_num),
+        m_size_per_head(size_per_head),
+        m_hidden_size(hidden_size),
+        m_intermediate_size(intermediate_size),
+        m_layer_norm_eps(layer_norm_eps),
+        m_max_position_embeddings(max_position_embeddings),
+        m_rotary_emb_base(rotary_emb_base),
+        m_rotary_pct(rotary_pct),
+        m_use_parallel_residual(use_parallel_residual),
+        m_vocab_size(vocab_size),
+        m_max_seq_len(max_seq_len),
+        m_cur_layer_num(cur_layer_num) {
+    constructor_validate_and_infer_types();
+}
+
+bool op::v10::GPTNeoxAttn::visit_attributes(AttributeVisitor& visitor) {
+    OV_OP_SCOPE(v10_GPTNeoxAttn_visit_attributes);
+    visitor.on_attribute("layer_num", m_layer_num);
+    visitor.on_attribute("head_num", m_head_num);
+    visitor.on_attribute("size_per_head", m_size_per_head);
+    visitor.on_attribute("hidden_size", m_hidden_size);
+    visitor.on_attribute("intermediate_size", m_intermediate_size);
+    visitor.on_attribute("layer_norm_eps", m_layer_norm_eps);
+    visitor.on_attribute("max_position_embeddings", m_max_position_embeddings);
+    visitor.on_attribute("rotary_emb_base", m_rotary_emb_base);
+    visitor.on_attribute("rotary_pct", m_rotary_pct);
+    visitor.on_attribute("use_parallel_residual", m_use_parallel_residual);
+    visitor.on_attribute("vocab_size", m_vocab_size);
+    visitor.on_attribute("max_seq_len", m_max_seq_len);
+    visitor.on_attribute("cur_layer_num", m_cur_layer_num);
+    return true;
+}
+
+void op::v10::GPTNeoxAttn::validate_and_infer_types() {
+    OV_OP_SCOPE(v10_GPTNeoxAttn_validate_and_infer_types);
+    // NODE_VALIDATION_CHECK(this,
+    //                       get_input_element_type(0).is_dynamic() || get_input_element_type(0).is_real(),
+    //                       "The element type of the input tensor must be a floating point number.");
+    // set_output_type(0, element::boolean, get_input_partial_shape(0));
+    auto ps = get_input_partial_shape(0);
+    ps.end()[-1] = m_hidden_size;
+    set_output_type(0, element::f32, ps);
+}
+
+std::shared_ptr<Node> op::v10::GPTNeoxAttn::clone_with_new_inputs(const OutputVector& new_args) const {
+    OV_OP_SCOPE(v10_GPTNeoxAttn_clone_with_new_inputs);
+    check_new_args_count(this, new_args);
+    return std::make_shared<op::v10::GPTNeoxAttn>(new_args.at(0), new_args.at(1), new_args.at(2),
+            m_layer_num,
+            m_head_num,
+            m_size_per_head,
+            m_hidden_size,
+            m_intermediate_size,
+            m_layer_norm_eps,
+            m_max_position_embeddings,
+            m_rotary_emb_base,
+            m_rotary_pct,
+            m_use_parallel_residual,
+            m_vocab_size,
+            m_max_seq_len,
+            m_cur_layer_num);
+}
+}  // namespace ov
